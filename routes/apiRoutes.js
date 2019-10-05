01521877806c81b38d3363f41ab83a9ea3342370
James <james.rudd45@gmail.com>
2019-10-05 10:33:57 +1000

    Final pres version

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,6 +38,21 @@ module.exports = function (app) {
       });
     }
   });
+
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    }
+    else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
   //----------- get all products from the soiled_db/products table -------
 
   app.get("/api/products", function (req, res) {
